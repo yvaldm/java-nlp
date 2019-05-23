ae8295d67b1845abc28047230fa6f43080023d33
valery.yakovlev <vd.yakovlev@mpoffice.ru>
2019-05-23 17:56:27 +0300

    initial impl of NLP feature extractor for products

---
+++ src/main/java/ru/mp/feature/extractor/nlpfeatureextractor/NlpFeatureExtractorApplication.java
@@ -0,0 +1,13 @@
+package ru.mp.feature.extractor.nlpfeatureextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpFeatureExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlpFeatureExtractorApplication.class, args);
+    }
+
+}
